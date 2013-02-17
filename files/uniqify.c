e203e04b7d08da3b971e7d21a80a6d8dbc3c701a
Colin Bradford <cjthehippo@gmail.com>
2013-02-16 19:19:56 -0800

    make compiler happen

--- files/uniqify.c
+++ files/uniqify.c
@@ -74,8 +74,8 @@
 
 // functions go here:
 void init_pipes(int num_pipes, int *pfd[]);
-void init_sort(int *pfd);
-void parser(int *pfd, int num_pipes);
+void init_sort(int **pfd, int **sfd);
+void parser(int **pfd, int num_pipes);
 FILE* merge_uniq(FILE *fpin, int cur, int max);
 FILE* mrg_two(FILE *fpin);
 
@@ -103,7 +103,7 @@ int main(int argc, char *argv[])
 	int num_pipes;
 	FILE *fpin;
 	FILE *fpout;
-	char *buf[MAXLINE];
+	char buf[MAXLINE];
 	
 	if(argc < 2) {
 		printf("uniqify: number of pipes is required.\n");
@@ -117,13 +117,14 @@ int main(int argc, char *argv[])
 	// set timer
 	
 	num_pipes = atoi(argv[1]);
+	fpin = (FILE *) malloc(num_pipes * sizeof(FILE));
 	init_pipes(num_pipes, pfd);
 	init_pipes(num_pipes, sfd);
 	
 	for(i = 0; i < num_pipes; i++) {
 		switch(fork()) {
 			case -1:
-				puke_exit("Fork");
+				puke_exit("Fork", PARENT);
 				break;
 			case  0:
 				init_sort(pfd);
@@ -140,7 +141,7 @@ int main(int argc, char *argv[])
 	for(i = 0; i < num_pipes; i++)
 		fpin[i] = fdopen(pfd[i][0], "r"); // open each stream
 	// remove duplicates
-	fpout = merge_uniq(fpin);
+	fpout = merge_uniq(fpin, 0, num_pipes - 1);
 	while(fgets(buf, MAXLINE, fpout))
 		printf("%s", buf);
 	//spike_stdin();
@@ -160,7 +161,7 @@ void init_pipes(int num_pipes, int *pfd[])
 	return;
 }
 
-void init_sort(int *pfd, int *sfd)
+void init_sort(int **pfd, int **sfd)
 {
 	dup2(pfd[0], STDIN_FILENO);
 	close(pfd[0]);
@@ -168,7 +169,7 @@ void init_sort(int *pfd, int *sfd)
 	
 	dup2(sfd[1], STDOUT_FILENO);
 	close(sfd[1]);
-	close(sfd[0])
+	close(sfd[0]);
 	
 	execlp("sort", "sort", (char *) NULL);
 }
@@ -176,13 +177,14 @@ void init_sort(int *pfd, int *sfd)
  * for now, just initializing the function
  * will add args and return values when appropriate
 **/
-void parser(int *pfd, int num_pipes)
+void parser(int **pfd, int num_pipes)
 {
 	// parse stuff
 	char buf[MAXLINE];
 	
 	int i;
 	int result;
+	FILE fpout[num_pipes];
 	
 	// need to parse stuff yo
 	
@@ -211,10 +213,10 @@ void parser(int *pfd, int num_pipes)
 FILE* merge_uniq(FILE *fpin, int cur, int max)
 {
 	if(cur == max)
-		return fpin[max];
+		return &(fpin[max]);
 	FILE merger[2];
 	merger[0] = fpin[cur];
-	merger[1] = merge_uniq(fpin, cur + 1, max);
+	merger[1] = *(merge_uniq(fpin, cur + 1, max));
 	return mrg_two(merger);
 }
 
@@ -227,7 +229,7 @@ FILE* mrg_two(FILE *fpin)
 	int x;
 	FILE new[2];
 	
-	if(!fgets(nxt, MAXLINE, fpin[0])) {
+	if(!fgets(nxt, MAXLINE, &fpin[0])) {
 		fclose(fpin[0]);
 		return fpin[1];
 	}
