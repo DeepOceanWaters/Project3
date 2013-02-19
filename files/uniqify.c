7398821256afe0178d1074ea3fbac5190ee7127f
Colin Bradford <cjthehippo@gmail.com>
2013-02-18 23:24:13 -0800

    Cleaned up code.

--- files/uniqify.c
+++ files/uniqify.c
@@ -1,74 +1,15 @@
-/**
- *
- * Divide Program into 3 types of processes:
- *	1. Single process that reads the input, parsing the lines into words.
- *	2. A group of processes does the sorting.
- *	3. Single process suppresses duplicate words & writes the output.
- *	
- * Must use the system sort command (/bin/sort) [Maybe use execve()?] with no
- * arguments to do the sorting. This program must arrange to start the processes
- * and plubm the pipes [no popen()]. The number of processes is a command line
- * argument to the program. The parser distributes the words round-robin to the
- * sorting processes.
- *	:: argv[1] or argv[2] == # of processes.
- * I/O to and from pipes => use the stream functions: fgets() & fputs()
- * Use fgets() & fputs() with fdopen() for attaching to pipes.
- * Use fclose() for flushing the streams.
- *
- * After each sort, much merge and uniqify words.
- *
- * Words are all:
- *	- Alphabetic
- *	- Case insensitive
- *	- Convert to lower [the parser should do this]
- *
- * All processes should terminate cleanly on the signals:
- *	- INTR
- * 	- QUIT
- *	- HANGUP
- *	[Use a signal handler: sigaction] [Do not use signal]
- *	[Ensure to issue QUIT signals to all children]
- *
- * Provide timings based on:
- * 	- Size of input file
- *	[Test with multiple types & sizes of files]
- *	[Include word lists & free form prose]
- *	[Timings should be plotted in a fashion that makes sense]
- *	[Recommended: matlab, gnuplot, R, or octave] [Use to generate plot]
- *	[Save the plot as a postscript file]
- *
- *
- * _________________EXTRA CREDIT________________________________________________
- *
- * Provide output similar to:
- *		uniq -c
- *	[Each word is output with its frequency]
- *	[For full credit: must calculate this "on the fly"]
- *	[This means: no storing all the words in memory]
- *
- * _____________________________________________________________________________
- *
- *
-**/
-	 
+
 /*__________________________HEADER STUFF______________________________________*/
 
+// includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <ctype.h>
-// more includes...
 
 // defines...
-#define SET_RED     	"\x1b[31m"
-#define SET_GREEN   	"\x1b[32m"
-#define SET_YELLOW  	"\x1b[33m"
-#define SET_BLUE    	"\x1b[34m"
-#define SET_MAGENTA 	"\x1b[35m"
-#define SET_CYAN    	"\x1b[36m"
-#define RESET_DA_COLOR  "\x1b[0m"
 #define MAXLINE		512
 #define PARENT		1
 #define CHILD		0
@@ -84,15 +25,7 @@ void rmdup(FILE *fpin);
 
 void puke_exit(char *msg, int type);
 
-// spikes go here:
-void spike_fork();
-void spike_pipe();
-void spike_sort();
-void spike_psrt();
-void spike_stdin();
 
-// test functions go here:
-void run_tests();
 
 /*_________________________END HEADER STUFF___________________________________*/
 
@@ -135,6 +68,23 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+/**
+ * pipe_children(FILE **fpin, int **pfd, int **sfd, int cur)
+ *
+ * Description:
+ *	Creates and initializes the pipes relavant to the child process about
+ *	to be fork()'d into existance. When the child is created, it closes all
+ *	irrelivant pipe file descriptors.
+ * FILE **fpin:
+ *	A FILE array of size num_pipes, this function uses this to initialize
+ *	a FILE in the array for the parent process.
+ * int **pfd:
+ *	The array of pipes pointing from parent to child.
+ * int **sfd:
+ *	The array of pipes pointing from child to parent.
+ * int cur:
+ *	The current child that is being created.
+**/
 void pipe_children(FILE **fpin, int **pfd, int **sfd, int cur)
 {
 	int j;
@@ -167,6 +117,17 @@ void pipe_children(FILE **fpin, int **pfd, int **sfd, int cur)
 	return;
 }
 
+/**
+ * init_sort(int *pfd, int *sfd)
+ *
+ * Description:
+ *	Called by a newly forked child - dups stdin/stdout appropriately then
+ *	closes all other pipes and exec()'s the sort command.
+ * int *pfd:
+ *	The pipe pointing from parent to child relevant to this child.
+ * int *sfd:
+ *	The pipe pointing from child to parent relevant to this child.
+**/
 void init_sort(int *pfd, int *sfd)
 {
 	//close(STDIN_FILENO);
@@ -187,8 +148,20 @@ void init_sort(int *pfd, int *sfd)
 }
 
 /**
- * for now, just initializing the function
- * will add args and return values when appropriate
+ * parser(int **pfd, int num_pipes)
+ *
+ * Description:
+ *	Opens the write end of the pipes going from the parent to its children,
+ *	then reads from stdin and calls parse_buf() to parse the input and 
+ *	round-robin place the parsed buffer into the opened write FILEs.
+ *	Finally, closes the write FILEs and returns.
+ *
+ * int **pfd:
+ *	An array of pipes; specifically the pipes that writes from the parent
+ *	to its children.
+ *
+ * int num_pipes:
+ *	Number of pipes specified by user.
 **/
 void parser(int **pfd, int num_pipes)
 {
@@ -211,6 +184,24 @@ void parser(int **pfd, int num_pipes)
 	return;
 }
 
+/**
+ * parse_buf(char *buf, FILE **fpout, int *i, int num_pipes)
+ *
+ * Description:
+ *	Used to parse a buffered line read from stdin. This function is used
+ * 	by parser(). Also round-robin reads the parsed buffer into the different
+ *	pipes.
+ * char *buf:
+ *	The buf read from stdin.
+ * FILE **fpout:
+ *	The different pipes, write end.
+ * int *i:
+ *	Pointer to an int to keep track of which fpout[] the function should be
+ * 	writing to.
+ * int num_pipes:
+ * 	The number of pipes specified by the user. Used to keep i from going out
+ * 	of bounds.
+**/
 void parse_buf(char *buf, FILE **fpout, int *i, int num_pipes)
 {
 	char new_buf[MAXLINE];
@@ -242,8 +233,18 @@ void parse_buf(char *buf, FILE **fpout, int *i, int num_pipes)
 }
 
 /**
+ * merge_uniq(FILE **fpin, int cur)
+ *
  * Description:
- *	Merges the information, and uniqifies the words.
+ *	Recursive function that merges two FILEs (with the help of mrg_two()),
+ *	and returns the result of the merged files. This result is used in 
+ *	previous calls to merge_uniq().
+ * FILE **fpin:
+ *	A FILE array the size of num_pipes (however many the user specifies).
+ * int cur:
+ *	Used to determine the current place in the array. Each recursive call
+ * 	to merge_uniq() subtracts 1 from cur until it reaches 0. When cur is
+ * 	0, merge_uniq() returns fpin[0].
  *
 **/
 FILE* merge_uniq(FILE **fpin, int cur)
@@ -257,6 +258,15 @@ FILE* merge_uniq(FILE **fpin, int cur)
 	return mrg_two(merger);
 }
 
+/**
+ * mrg_two(FILE **fpin)
+ *
+ * Description:
+ * 	mrg_two() takes a FILE array of size two and merges the two FILEs into
+ * 	a single pipe. Used in conjunction with merge_uniq().
+ * FILE **fpin:
+ *	A FILE array of size two. This contains the two FILEs to be merged.
+**/
 FILE* mrg_two(FILE **fpin)
 {
 	char cur[MAXLINE];
@@ -305,10 +315,14 @@ FILE* mrg_two(FILE **fpin)
 }
 
 /**
- * rmdup => remove duplicates
+ * rmdup(FILE *fpout)
  *
- * for now, just initializing the function
- * will add args and return values when appropriate
+ * Description:
+ *	Takes a sorted pipe, reads from it, and then uniqifies it and counts
+ *	the number of times a word appears like uniq -c.
+ * FILE *fpout:
+ * 	The file that points to the read end of the final merged and sorted
+ *	pipe.
 **/
 void rmdup(FILE *fpout)
 {
@@ -336,6 +350,8 @@ void rmdup(FILE *fpout)
 }
 
 /**
+ * puke_exit(char *msg, int type)
+ *
  * Description:
  *	Takes a message, and pukes & exits. If process type is unknown, puke
  *	error and continue the process.
@@ -352,6 +368,7 @@ void rmdup(FILE *fpout)
 void puke_exit(char *msg, int type)
 {
 	perror(msg);
+	
 	if(type == PARENT)
 		exit(EXIT_FAILURE);
 	else if(type == CHILD)
@@ -360,265 +377,6 @@ void puke_exit(char *msg, int type)
 		printf("%s: unknown process type - continuing\n", msg);
 	return;
 }
-
-
-// Spikes
-
-void spike_fork()
-{
-	int result;
-	int status;
-	int i;
-	int child_pid;
-	char buf[MAXLINE];
-	
-	char *colors[6] = { SET_RED, SET_GREEN, SET_YELLOW, SET_BLUE,
-		SET_MAGENTA, SET_CYAN };
-		
-	FILE *input;
-	FILE *output;
-	
-	int pipefds[2];
-	
-	printf("spike_fork: starting spike test\n");
-	if(pipe(pipefds) != 0){
-		perror("Pipes are screwed up, call a plumber");
-		exit(-1);
-	}
-	
-	printf("spike_fork: starting spike test\n");
-	
-	for(i = 0; i < 6; i++) {
-		printf("\nspike_fork: PARENT: creating %sCHILD[%d]" 
-			RESET_DA_COLOR "\n", colors[i], i);
-		
-		switch((result = fork())){
-	
-		case -1:
-			//in parent, oops
-			printf("spike_fork:  PARENT: forking error\n");
-			perror("Forking failed");
-			exit(EXIT_FAILURE);
-			break;
-		case 0:
-			//child case
-			close(pipefds[0]);
-			printf("spike_fork: %sCHILD[%d]" RESET_DA_COLOR 
-				": writing to pipe\n", colors[i], i);
-			input = fdopen(pipefds[1], "w");
-			fputs(colors[i], input);
-			fputs("\n", input);
-			printf("spike_fork: %sCHILD[%d]"
-				RESET_DA_COLOR ": exiting\n", colors[i], i);
-			fclose(input);
-			_exit(0);
-			break;
-		default:
-			//parent case -- result holds pid of child
-			
-			break;
-		}
-	}
-	
-	close(pipefds[1]);
-	output = fdopen(pipefds[0], "r");
-	// probably won't work the way I want it to, i.e. fdopen truncates
-	while((child_pid = wait(&status)) != -1) {
-		fgets(buf, MAXLINE, output);
-		printf("%sCHILD[] finished..." RESET_DA_COLOR "\n", buf);
-	}
-	fclose(output);
-	printf("spike_fork: finished\n");
-	if(errno != ECHILD) {
-		perror("Error with child");
-		exit(EXIT_FAILURE);
-	}
-	return;
-}
-
-void spike_pipe()
-{
-	char line[MAXLINE];
-	FILE *fpin;
-	FILE *fpout;
-	int result;
-	int fd[2];
-	int i;
-	int status;
-
-	pipe(fd);
-
-	switch((result = fork())) {
-
-	case 0: 
-		close(fd[0]);
-	
-		fpout = fdopen(fd[1], "w");
-	
-		for(i = 0; i < 10; i++)
-			fputs("jeronimooo...", fpout);
-	
-		fclose(fpout);
-		_exit(EXIT_SUCCESS);
-		break;
-	default:
-		close(fd[1]);
-	
-		fpin = fdopen(fd[0], "r");
-		wait(&status);
-		while(fgets(line, MAXLINE, fpin) != NULL)
-			printf("%d: %s", i++, line);
-	
-		fclose(fpin);
-		break;
-	}
-	
-	printf("spike_pipe:  PARENT: spike test finished\n");
-	return;
-}
-
-void spike_sort()
-{
-	int pfd[2];
-	FILE *fpout;
-	char sort_this[] = "ant\nbeegle\nzanzibar\nmoo\ncow\nnope\nhit\npork";
-	
-	pipe(pfd);
-	
-	fpout = fdopen(pfd[1], "w");
-	fputs(sort_this, fpout);
-	fclose(fpout);
-	
-	dup2(pfd[0], STDIN_FILENO);
-	close(pfd[0]);
-	dup2(pfd[1], STDOUT_FILENO);
-	close(pfd[1]);
-	
-	execlp("sort", "sort", (char *) NULL);
-	
-	return;
-}
-
-void spike_psrt()
-{
-	int pfd[2];
-	FILE *fpout;
-	FILE *fpin;
-	
-	int result;
-	int i;
-	char buf[MAXLINE];
-	char *colors[6] = { SET_RED, SET_GREEN, SET_YELLOW, SET_BLUE,
-		SET_MAGENTA, SET_CYAN };
-	
-	char *sort_this[] = { "ant\n", "beegle\n", "zanzibar\n", "moo\n",
-		"cow\n", "nope\n", "hit\n", "pork\n" };
-	
-	pipe(pfd);
-	
-	for(i = 0; i < 6; i++) {
-		switch((result = fork())) {
-		case -1:
-			// parent effed up yo
-			perror("oh no");
-			exit(EXIT_FAILURE);
-			break;
-		case  0:
-			// child case
-			close(pfd[0]);
-			fpin = fdopen(pfd[1], "r");
-			fgets(buf, MAXLINE, fpin);
-			printf("%sCHILD[%d]: read: %s " RESET_DA_COLOR 
-				"\n", colors[i], i, buf);
-			fclose(fpin);
-			//execlp("sort", "sort",  (char*) NULL);
-			_exit(EXIT_FAILURE);
-			break;
-		default:
-			// parent
-			break;
-		}
-	}
-	close(pfd[0]);
-	fpout = fdopen(pfd[1], "w");
-	for(i = 0; i < 8; i++)
-		fputs(sort_this[i], fpout);
-	fclose(fpout);
-	
-	return;
-}
-
-void spike_stdin()
-{
-	/*int pfd[2];
-	int pfd2[2];
-	int i;
-	FILE *fpout;
-	FILE *fpin;
-	
-	char buf[MAXLINE];
-	
-	char *old;
-	char *content;
-	
-	int cnt_size;
-	int result;
-	
-	pipe(pfd);
-	pipe(pfd2);
-	
-	for(i = 0; i < 3; i++) {
-	switch((result = fork())) {
-	case -1:
-		// parent effed up yo
-		perror("oh no");
-		exit(EXIT_FAILURE);
-		break;
-	case  0:
-		// child case
-		dup2(pfd[i][0], STDIN_FILENO);
-		close(pfd[i][0]);
-		dup2(pfd2[i][1], STDOUT_FILENO);
-		close(pfd2[i][1]);
-		close(pfd[i][1]);
-		close(pfd2[i][0]);
-		
-		execlp("sort", "sort",  (char*) NULL);
-		_exit(EXIT_FAILURE);
-		break;
-	default:
-		// parent
-		break;
-	}
-	}
-	
-	cnt_size = 0;
-	
-	close(pfd[i][0]);
-	close(pfd2[i][1]);
-	fpout = fdopen(pfd[1], "w");
-		
-	while(fgets(buf, MAXLINE, stdin))
-		fputs(buf, fpout);
-	fclose(fpout);
-	
-	printf("Done writing, sorting now...\n");
-	fpin = fdopen(pfd2[0], "r");
-	while(fgets(buf, MAXLINE, fpin))
-		printf("%s\n", buf);
-	fclose(fpin);
-	
-	return;	//*/
-}
-// Tests
-
-void run_tests()
-{
-	
-}
-
-
-
 
 
 
